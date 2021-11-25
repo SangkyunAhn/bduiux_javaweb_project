9b412d5ecccaef671e66604205e8cc20e3c9da95
DESKTOP-B0KVD9C\504 <sangkyun4789@naver.com>
2021-11-25 16:27:29 +0900

    AWS migration

--- ahn/project_2/src/main/java/com/bookshop01/common/base/BaseController.java
+++ ahn/project_2/src/main/java/com/bookshop01/common/base/BaseController.java
@@ -26,7 +26,7 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 import com.bookshop01.goods.vo.ImageFileVO;
 
 public abstract class BaseController  {
-	private static final String CURR_IMAGE_REPO_PATH = "C:/shopping/file_repo";
+	private static final String CURR_IMAGE_REPO_PATH = "/home/ubuntu/shopping/file_repo";
 	BoardService boardService;
 
 	protected List<ImageFileVO> upload(MultipartHttpServletRequest multipartRequest) throws Exception{
