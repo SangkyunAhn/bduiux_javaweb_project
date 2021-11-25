06880110f3db0e3df68990b32383badfe472e0c1
SangkyunAhn <sangkyun4789@naver.com>
2021-11-25 09:32:08 +0900

    Error related to deleting article is solved.

--- pro30/src/main/java/com/myspring/pro30/board/controller/BoardControllerImpl.java
+++ pro30/src/main/java/com/myspring/pro30/board/controller/BoardControllerImpl.java
@@ -120,22 +120,6 @@ public class BoardControllerImpl  implements BoardController{
 		return mav;
 	}
 
-	/*
-	//다중 이미지 보여주기
-	@RequestMapping(value="/board/viewArticle.do" ,method = RequestMethod.GET)
-	public ModelAndView viewArticle(@RequestParam("articleNO") int articleNO,
-			  HttpServletRequest request, HttpServletResponse response) throws Exception{
-		String viewName = (String)request.getAttribute("viewName");
-		Map articleMap=boardService.viewArticle(articleNO);
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName(viewName);
-		mav.addObject("articleMap", articleMap);
-		return mav;
-	}
-   */
-
-
-
 	//한 개 이미지 수정 기능
 	@RequestMapping(value="/board/modArticle.do" ,method = RequestMethod.POST)
 	@ResponseBody
@@ -222,87 +206,6 @@ public class BoardControllerImpl  implements BoardController{
 		return resEnt;
 	}
 
-/*
-  //다중 이미지 글 추가하기
-  @Override
-  @RequestMapping(value="/board/addNewArticle.do" ,method = RequestMethod.POST)
-  @ResponseBody
-  public ResponseEntity  addNewArticle(MultipartHttpServletRequest multipartRequest, HttpServletResponse response) throws Exception {
-	multipartRequest.setCharacterEncoding("utf-8");
-	String imageFileName=null;
-
-	Map articleMap = new HashMap();
-	Enumeration enu=multipartRequest.getParameterNames();
-	while(enu.hasMoreElements()){
-		String name=(String)enu.nextElement();
-		String value=multipartRequest.getParameter(name);
-		articleMap.put(name,value);
-	}
-
-	//로그인 시 세션에 저장된 회원 정보에서 글쓴이 아이디를 얻어와서 Map에 저장합니다.
-	HttpSession session = multipartRequest.getSession();
-	MemberVO memberVO = (MemberVO) session.getAttribute("member");
-	String id = memberVO.getId();
-	articleMap.put("id",id);
-
-
-	List<String> fileList =upload(multipartRequest);
-	List<ImageVO> imageFileList = new ArrayList<ImageVO>();
-	if(fileList!= null && fileList.size()!=0) {
-		for(String fileName : fileList) {
-			ImageVO imageVO = new ImageVO();
-			imageVO.setImageFileName(fileName);
-			imageFileList.add(imageVO);
-		}
-		articleMap.put("imageFileList", imageFileList);
-	}
-	String message;
-	ResponseEntity resEnt=null;
-	HttpHeaders responseHeaders = new HttpHeaders();
-    responseHeaders.add("Content-Type", "text/html; charset=utf-8");
-	try {
-		int articleNO = boardService.addNewArticle(articleMap);
-		if(imageFileList!=null && imageFileList.size()!=0) {
-			for(ImageVO  imageVO:imageFileList) {
-				imageFileName = imageVO.getImageFileName();
-				File srcFile = new File(ARTICLE_IMAGE_REPO+"\\"+"temp"+"\\"+imageFileName);
-				File destDir = new File(ARTICLE_IMAGE_REPO+"\\"+articleNO);
-				//destDir.mkdirs();
-				FileUtils.moveFileToDirectory(srcFile, destDir,true);
-			}
-		}
-
-		message = "<script>";
-		message += " alert('새글을 추가했습니다.');";
-		message += " location.href='"+multipartRequest.getContextPath()+"/board/listArticles.do'; ";
-		message +=" </script>";
-	    resEnt = new ResponseEntity(message, responseHeaders, HttpStatus.CREATED);
-
-
-	}catch(Exception e) {
-		if(imageFileList!=null && imageFileList.size()!=0) {
-		  for(ImageVO  imageVO:imageFileList) {
-		  	imageFileName = imageVO.getImageFileName();
-			File srcFile = new File(ARTICLE_IMAGE_REPO+"\\"+"temp"+"\\"+imageFileName);
-		 	srcFile.delete();
-		  }
-		}
-
-
-		message = " <script>";
-		message +=" alert('오류가 발생했습니다. 다시 시도해 주세요');');";
-		message +=" location.href='"+multipartRequest.getContextPath()+"/board/articleForm.do'; ";
-		message +=" </script>";
-		resEnt = new ResponseEntity(message, responseHeaders, HttpStatus.CREATED);
-		e.printStackTrace();
-	}
-	return resEnt;
-  }
-
-*/
-
-
-
 	@RequestMapping(value = "/board/*Form.do", method =  RequestMethod.GET)
 	private ModelAndView form(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String viewName = (String)request.getAttribute("viewName");
@@ -333,27 +236,4 @@ public class BoardControllerImpl  implements BoardController{
 		return imageFileName;
 	}
 
-	/*
-	//다중 이미지 업로드하기
-	private List<String> upload(MultipartHttpServletRequest multipartRequest) throws Exception{
-		List<String> fileList= new ArrayList<String>();
-		Iterator<String> fileNames = multipartRequest.getFileNames();
-		while(fileNames.hasNext()){
-			String fileName = fileNames.next();
-			MultipartFile mFile = multipartRequest.getFile(fileName);
-			String originalFileName=mFile.getOriginalFilename();
-			fileList.add(originalFileName);
-			File file = new File(ARTICLE_IMAGE_REPO +"\\"+ fileName);
-			if(mFile.getSize()!=0){ //File Null Check
-				if(! file.exists()){ //경로상에 파일이 존재하지 않을 경우
-					if(file.getParentFile().mkdirs()){ //경로에 해당하는 디렉토리들을 생성
-							file.createNewFile(); //이후 파일 생성
-					}
-				}
-				mFile.transferTo(new File(ARTICLE_IMAGE_REPO +"\\"+"temp"+ "\\"+originalFileName)); //임시로 저장된 multipartFile을 실제 파일로 전송
-			}
-		}
-		return fileList;
-	}
-	*/
 }
